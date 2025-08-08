c1132bac2c3442e5df9f098680f910875a972ea6
Topelus <143185460+Topelus@users.noreply.github.com>
2025-08-08 21:30:16 +0100

    mods

---
+++ assets/scripts/api.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+// Connexion à la BDD avec variables d'environnement
+$servername = getenv("DB_HOST");
+$username   = getenv("DB_USER");
+$password   = getenv("DB_PASS");
+$dbname     = getenv("DB_NAME");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo json_encode(["error" => "Connexion échouée : " . $conn->connect_error]);
+    exit;
+}
+
+$result = $conn->query("SELECT * FROM ma_table");
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+
+$conn->close();
+?>
